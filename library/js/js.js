9379decd3d36c73d83af34a1dd830552ff91ce4d
techworldthink <jobin25051999@gmail.com>
2020-12-11 17:10:14 +0530

    initial again

--- library/js/js.js
+++ library/js/js.js
@@ -5,18 +5,17 @@ function find() {
   let c1 = eval(w1 * n1 * h1);
   document.getElementById("c1").value = c1;
 
-  let w1 = document.getElementById("w2").value;
-  let n1 = document.getElementById("n2").value;
-  let h1 = document.getElementById("h2").value;
-  let c1 = eval(w2 * n2 * h2);
+  let w2 = document.getElementById("w2").value;
+  let n2 = document.getElementById("n2").value;
+  let h2 = document.getElementById("h2").value;
+  let c2 = eval(w2 * n2 * h2);
   document.getElementById("c2").value = c2;
 
   let total = 0;
   let cons = 0;
   total = c1 + c2;
   cons = total/1000;
-  
-
+console.log(cons)
 }
 
 
