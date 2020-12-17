b8b69f6e3b7cbecf35df3aaf0c92b64a443e7b9f
DANY ROBERT <rtdany10@gmail.com>
2020-12-17 12:32:23 +0530

    Update energy.py

--- V_3-Frappe/python-app-file/energy.py
+++ V_3-Frappe/python-app-file/energy.py
@@ -7,32 +7,17 @@ import frappe
 from frappe.model.document import Document
 
 class energy(Document):
-        def ener():
-            return "haloiii"
-                
-	
+	pass
+
 @frappe.whitelist()
-def ping1(doctype,app_name,com_name,mod_name,wat_name):
-        app=app_name.strip('][').split(',')
-        com=com_name.strip('][').split(',')
-        mod=mod_name.strip('][').split(',')
-        wat=wat_name.strip('][').split(',')
-        count=len(app)
-        for i in range(count):
-                doc = frappe.get_doc({
-                        'doctype':'energy',
-                         'app':app[i],
-                         'com':com[i],
-                         'mod':mod[i],
-                         'watts':wat[i]
-                        })
-                doc.insert(
-                        ignore_permissions=True,
-                        ignore_links=True,
-                        ignore_if_duplicate=True,
-                        ignore_mandatory=True
-                        )
-        return 1;  
-
-             
-
+def ping1(doctype, data={}):
+	for key in data:
+        doc = frappe.get_doc({
+			'doctype': doctype,
+			'app': data[key][0],
+			'com': data[key][1],
+			'mod': data[key][2],
+			'watts': data[key][3]
+		})
+        doc.insert(ignore_permissions=True, ignore_if_duplicate=True, ignore_mandatory=True)
+	return
